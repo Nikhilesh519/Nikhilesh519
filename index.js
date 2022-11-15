eb5cf5044e3f0ce8c4c1ccd3185237a43397398f
Nikhilesh519 <115639178+Nikhilesh519@users.noreply.github.com>
2022-11-15 22:13:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// 1.The 'var' and 'function' are knowns as Data Declaration
+// explaination:The "function" and "var" both are the Declaration statements.
+// These both are used for defining, and declaring variable, function in anywhere in the program.
+
+
+// 2.Which of these is a correct output for the JavaScript code given below?  
+String a= "Hey";
+String b="There";
+ alert(a+b);
